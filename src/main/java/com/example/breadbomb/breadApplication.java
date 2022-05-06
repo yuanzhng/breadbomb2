07e49b32a1cd4b033b5921663f2d53dd69735551
Daniel Chen <ychen@ucls.uchicago.edu>
2022-05-06 14:16:48 -0500

    b

--- src/main/java/com/example/breadbomb/breadApplication.java
+++ src/main/java/com/example/breadbomb/breadApplication.java
@@ -11,7 +11,8 @@ public class breadApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(breadApplication.class.getResource("breadcontroller-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        stage.setResizable(false);
         stage.setTitle("midbomb");
         stage.setScene(scene);
         stage.show();
