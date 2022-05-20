774ef14ce56a3f9c1cb416647a41701e708204fe
Dan Wheadon <dwheadon@gmail.com>
2022-05-20 13:45:34 -0500

    Allow javafx-media to be used in the project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.breadbomb {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.media;
     requires org.controlsfx.controls;
 
     opens com.example.breadbomb to javafx.fxml;
